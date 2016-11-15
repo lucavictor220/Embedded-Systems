4b7f56a1f46d3b8c44bec7d4295d34e8d7f8192f
vitiok <lucavictor220@gmail.com>
2016-11-15 21:18:40 +0200

    Added template for Lab2.

---
+++ Lab2/Lab2/headers/uart_stdio.h
@@ -0,0 +1,21 @@
+/*
+ * uart_stdio.h
+ *
+ * Created: 11/6/2016 1:05:08 PM
+ *  Author: Vitiok
+ */ 
+
+
+#ifndef _UART_STDIO_H
+#define _UART_STDIO_H
+
+#define UART_BAUD 9600
+#define F_CPU 1000000UL
+
+#include <stdio.h>
+#include <avr/io.h>
+
+void uart_stdio_Init(void);
+void uart_PutChar(char c, FILE *stream);
+
+#endif
